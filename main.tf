5a86a13177d4342157b479ce91f63ce335a5eff6
vickythebest <50752860+vickythebest@users.noreply.github.com>
2023-04-26 10:07:01 -0700

    customer security group

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids =[module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
     
   tags = {
     Name = "HelloWorld"
@@ -32,15 +32,15 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
-  name = "blog"
+  name    = "blog"
   
-  vpc_id = data.aws.vpc.default.id
+  vpc_id = data.aws_vpc.default.id
   
-  ingress_rules =["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks=["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
   
-  egress_rules =["all-all"]
-  egress_cidr_blocks=["0.0.0.0/0"]
+  egress_rules         = ["all-all"]
+  egress_cidr_blocks   = ["0.0.0.0/0"]
 }
   
 
