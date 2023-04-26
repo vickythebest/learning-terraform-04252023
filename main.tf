40315d5f5605f4d5e5d0502c52a82f039caec2cd
vickythebest <50752860+vickythebest@users.noreply.github.com>
2023-04-26 12:17:12 -0700

    Refactor to use variable

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,24 +11,24 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -37,9 +37,9 @@ module "autoscaling" {
   version = "6.9.0"
   # insert the 1 required variable here
 
-  name = "blog"
-  min_size =1
-  max_size=2
+  name = "${var.environment.name}.blog"
+  min_size var.asg_min_size
+  max_size=var.asg_max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
@@ -53,7 +53,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
-  name = "blog-alb"
+  name = "${var.environment.name}.blog-alb"
 
   load_balancer_type = "application"
 
@@ -63,7 +63,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog"
+      name_prefix      = "${var.environment.name}"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -79,14 +79,14 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = "${var.environment.name}"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
-  name    = "blog"
+  name    = "${var.environment.name}.blog"
   
   vpc_id = module.blog_vpc.public_subnets[0]
   
