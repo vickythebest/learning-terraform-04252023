bd0cac4b20739637e7d95f55f780aee3e0584071
vickythebest <50752860+vickythebest@users.noreply.github.com>
2023-04-26 12:25:11 -0700

    Refactor to use variable

--- variables.tf
+++ variables.tf
@@ -4,12 +4,12 @@ variable "instance_type" {
 }
 
 variable "ami_filter"{
-  description= "name filter and owner for AMI"
-    
-  type = object{{
+  description= "Name filter and owner for AMI"
+
+  type = object({
     name  = string
     owner = string
-  }}
+  })
 
   default = {
   name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
@@ -20,10 +20,10 @@ variable "ami_filter"{
 variable ="environment"{
   description ="Development environment"
 
-  type = object{{
+  type = object({
     name = string
     network_prefix = string
-  }}
+  })
   
   default={
     name = "dev"
