cd7404856cda3fc1abf961a67417a48a161a47b3
vickythebest <50752860+vickythebest@users.noreply.github.com>
2023-04-26 09:30:51 -0700

    Use module for security group

--- main.tf
+++ main.tf
@@ -22,12 +22,27 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids =[aws_security_group.blog.id]
+  vpc_security_group_ids =[module.blog_blog_sg.security_group_id]
+    
   tags = {
     Name = "HelloWorld"
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+  name = "blog_new"
+  
+  security_group_id = aws_security_group.blog.id 
+  
+  ingress_rule =["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks=["0.0.0.0/0"]
+  
+  egress_rule =["all-all"]
+  egress_cidr_blocks=["0.0.0.0/0"]
+}
+  
 resource "aws_security_group" "blog"{
   name = "blog"
   description = "Allow http and htttps in allow everything out"
