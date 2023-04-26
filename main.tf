b876ca106e5ab45659b510d5097b02ae45c35418
vickythebest <50752860+vickythebest@users.noreply.github.com>
2023-04-26 11:46:30 -0700

    autoscalling

--- main.tf
+++ main.tf
@@ -32,18 +32,24 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.9.0"
+  # insert the 1 required variable here
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-    
-  tags = {
-    Name = "HelloWorld"
-  }
+  name = "blog"
+  min_size =1
+  max_size=2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+  
+  image_id           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
